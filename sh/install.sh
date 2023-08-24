e011d7c740e813dbc179520bd09986c2a49bef6d
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2023-08-24 15:54:45 +0800

    Update install.sh

--- sh/install.sh
+++ sh/install.sh
@@ -50,7 +50,7 @@ echo '{}' > ${Set_ConfDir}/config.json
 
 curl https://raw.githubusercontent.com/SagerNet/sing-box/dev-next/release/config/sing-box.service | sed -e "s@${Default_ConfDir}@${Set_ConfDir}@g;s@${Default_BinDir}@${Set_BinDir}@g" > /etc/systemd/system/sing-box.service
 
-wget -O /etc/systemd/system/sing-box@.service https://raw.githubusercontent.com/SagerNet/sing-box/dev-next/release/config/sing-box@.service
+curl https://raw.githubusercontent.com/SagerNet/sing-box/dev-next/release/config/sing-box@.service | sed -e "s@${Default_ConfDir}@${Set_ConfDir}@g;s@${Default_BinDir}@${Set_BinDir}@g" > /etc/systemd/system/sing-box@.service
 
 systemctl daemon-reload
 
