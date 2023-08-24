825ba28d8ecd831b3df8093c990c443b6ad0edc7
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2023-08-24 13:44:13 +0800

    Update install.sh

--- sh/install.sh
+++ sh/install.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
+echo '正在安装/升级必需插件. . .'
+
 apt update
 
-apt install -y wget tar
+apt install -y curl wget tar
+
+echo '正在安装/升级go. . .'
+
+apt remove -y --purge golang
+
+apt autoremove -y
 
 rm -rf /usr/local/go
 
@@ -16,5 +24,28 @@ export PATH=$PATH:/usr/local/go/bin
 
 go version
 
-go install -v -tags with_quic with_dhcp with_wireguard with_utls with_reality_server with_acme with_gvisor github.com/sagernet/sing-box/cmd/sing-box@latest
+echo '正在编译sing-box核心. . .'
 
+Singbox_Version=$(curl https://api.github.com/repos/SagerNet/sing-box/releases  | grep "tag_name" | head -n 1 | awk -F\" '{print $4}')
+
+go install -v -tags with_quic,with_dhcp,with_wireguard,with_utls,with_reality_server,with_acme,with_gvisor github.com/sagernet/sing-box/cmd/sing-box@${Singbox_Version}
+
+echo '正在安装sing-box核心. . .'
+
+mv -f /root/go/bin/sing-box /usr/local/bin/
+
+Default_ConfDir=/etc/sing-box
+
+Set_ConfDir=/usr/local/etc/sing-box
+
+mkdir -p ${Set_ConfDir}
+
+wget -O ${Set_ConfDir}/config.json https://raw.githubusercontent.com/SagerNet/sing-box/dev-next/release/config/config.json
+
+curl https://raw.githubusercontent.com/SagerNet/sing-box/dev-next/release/config/sing-box.service | sed -e "s@${Default_ConfDir}@${Set_ConfDir}@g" > /etc/systemd/system/sing-box.service
+
+wget -O /etc/systemd/system/sing-box@.service https://raw.githubusercontent.com/SagerNet/sing-box/dev-next/release/config/sing-box@.service
+
+systemctl daemon-reload
+
+echo 'sing-box核心已安装!'
