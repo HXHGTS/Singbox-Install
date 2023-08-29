db1d14b344f09f497aa29f710ac864d065fe1936
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2023-08-29 20:50:37 +0800

    Create uninstall.sh

---
+++ sh/uninstall.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+echo '正在关闭/卸载sing-box核心. . .'
+
+systemctl stop sing-box
+
+systemctl disable sing-box
+
+Default_ConfDir=/etc/sing-box
+
+Set_ConfDir=/usr/local/etc/sing-box
+
+Default_BinDir=/usr/bin/sing-box
+
+Set_BinDir=/usr/local/bin/sing-box
+
+rm -f /root/go/bin/sing-box ${Set_BinDir}
+
+rm -rf ${Set_ConfDir}
+
+rm -f /etc/systemd/system/sing-box.service /etc/systemd/system/sing-box@.service
+
+systemctl daemon-reload
+
+echo 'sing-box核心已卸载!'
+
+exit 0
