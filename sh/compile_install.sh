c14d36f4bb31a895cb778918dacbe80a77b2f59e
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2023-09-01 17:41:24 +0800

    Update compile_install.sh

--- sh/compile_install.sh
+++ sh/compile_install.sh
@@ -40,7 +40,7 @@ Singbox_Version=$(curl https://api.github.com/repos/SagerNet/sing-box/releases  
 
 env GOOS=linux GOARCH=amd64 GOAMD64=v3 CGO_ENABLED=1 go install -ldflags "-X github.com/sagernet/sing-box/constant/version.Version=${Singbox_Version}" -v -tags with_quic,with_grpc,with_dhcp,with_wireguard,with_shadowsocksr,with_ech,with_utls,with_reality_server,with_acme,with_clash_api,with_v2ray_api,with_gvisor,with_lwip github.com/sagernet/sing-box/cmd/sing-box@${Singbox_Version}
 
-#go install -v -tags with_quic,with_grpc,with_dhcp,with_wireguard,with_shadowsocksr,with_ech,with_utls,with_reality_server,with_acme,with_clash_api,with_v2ray_api,with_gvisor,with_embedded_tor,with_lwip github.com/sagernet/sing-box/cmd/sing-box@${Singbox_Version}
+#go install -ldflags "-X github.com/sagernet/sing-box/constant/version.Version=${Singbox_Version}" -v -tags with_quic,with_grpc,with_dhcp,with_wireguard,with_shadowsocksr,with_ech,with_utls,with_reality_server,with_acme,with_clash_api,with_v2ray_api,with_gvisor,with_embedded_tor,with_lwip github.com/sagernet/sing-box/cmd/sing-box@${Singbox_Version}
 
 echo '正在安装sing-box核心. . .'
 
