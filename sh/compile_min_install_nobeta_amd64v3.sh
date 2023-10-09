e8f494d144d90b0c20490de77176fc9f74178973
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2023-10-09 19:23:02 +0800

    Update compile_min_install_nobeta_amd64v3.sh

--- sh/compile_min_install_nobeta_amd64v3.sh
+++ sh/compile_min_install_nobeta_amd64v3.sh
@@ -34,9 +34,7 @@ go version
 
 echo '正在编译linux版sing-box核心. . .'
 
-Singbox_Version=$(curl https://api.github.com/repos/SagerNet/sing-box/releases  | grep "tag_name" | grep -v 'beta' | grep -v 'rc' | head -n 1 | gawk -F\" '{print $4}')
-
-env GOOS=linux GOARCH=amd64 GOAMD64=v3 CGO_ENABLED=0 go install -ldflags "-X github.com/sagernet/sing-box/constant.Version=${Singbox_Version}" -v -tags with_quic,with_grpc,with_dhcp,with_wireguard,with_ech,with_utls,with_reality_server,with_acme,with_gvisor github.com/sagernet/sing-box/cmd/sing-box@${Singbox_Version}
+env GOOS=linux GOARCH=amd64 GOAMD64=v3 CGO_ENABLED=0 go install -ldflags "-X github.com/sagernet/sing-box/constant.Version=${Singbox_Version}" -v -tags with_quic,with_grpc,with_dhcp,with_wireguard,with_ech,with_utls,with_reality_server,with_acme,with_gvisor github.com/sagernet/sing-box/cmd/sing-box@@latest
 
 echo '正在安装sing-box核心. . .'
 
