5f92327455e09d4143603613da1ef6c8fed45e42
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2023-10-31 20:12:06 +0800

    Update win64clientmake_nobeta.sh

--- sh/win64clientmake_nobeta.sh
+++ sh/win64clientmake_nobeta.sh
@@ -4,6 +4,8 @@ export PATH=$PATH:/usr/local/go/bin
 
 echo '正在编译Windows版sing-box核心. . .'
 
+Singbox_Version=$(curl https://api.github.com/repos/SagerNet/sing-box/releases  | grep "tag_name" | grep -v '-'| head -n 1 | gawk -F\" '{print $4}')
+
 env GOOS=windows GOARCH=amd64 CGO_ENABLED=0 go install -ldflags "-X github.com/sagernet/sing-box/constant.Version=${Singbox_Version}" -v -tags with_dhcp,with_utls,with_reality_server github.com/sagernet/sing-box/cmd/sing-box@latest
 
 echo 'sing-box Windows版客户端已编译完成!'
