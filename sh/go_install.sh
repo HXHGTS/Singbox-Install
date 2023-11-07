c57f2f0f82cf191a98fd87008a8b05d135295e67
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2023-11-07 12:50:34 +0800

    Create go_install.sh

---
+++ sh/go_install.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+echo '正在安装/升级必需插件. . .'
+
+apt update
+
+apt install -y curl wget tar gawk sed
+
+echo '正在安装/升级go. . .'
+
+apt remove -y --purge golang
+
+apt autoremove -y
+
+rm -rf /usr/local/go
+
+Go_Version=$(curl https://github.com/golang/go/tags | grep '/releases/tag/go' | head -n 1 | gawk -F/ '{print $6}' | gawk -F\" '{print $1}')
+
+wget -O /var/tmp/${Go_Version}.linux-amd64.tar.gz https://go.dev/dl/${Go_Version}.linux-amd64.tar.gz
+
+tar -C /usr/local -xzf /var/tmp/${Go_Version}.linux-amd64.tar.gz
+
+rm -f /var/tmp/${Go_Version}.linux-amd64.tar.gz
+
+export PATH=$PATH:/usr/local/go/bin
+
+echo -e 'export PATH=$PATH:/usr/local/go/bin\nexit 0\n' > /etc/rc.local
+
+chmod +x /etc/rc.local
+
+systemctl daemon-reload
+
+systemctl start rc-local
+
+systemctl enable rc-local
+
+go version
+
+exit 0
